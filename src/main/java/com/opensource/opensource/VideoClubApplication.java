a1e25840d05e9d13739656a94c23b19cd7fe704e
keyrol <keyrol1222@gmail.com>
2024-01-17 17:56:32 -0400

    first commit

---
+++ src/main/java/com/opensource/opensource/VideoClubApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.opensource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoClubApplication.class, args);
+	}
+
+}
