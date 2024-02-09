455cb84818e3dda5fc63c8d760b13d0799bd334f
2003-Miguel <156967679+2003-Miguel@users.noreply.github.com>
2024-02-08 22:56:40 -0400

    .

--- src/main/java/com/opensource/Persistence/exceptions/NonexistentEntityException.java
+++ src/main/java/com/opensource/Persistence/exceptions/NonexistentEntityException.java
@@ -1,4 +1,4 @@
-package Persistence.exceptions;
+package com.opensource.Persistence.exceptions;
 
 public class NonexistentEntityException extends Exception {
     public NonexistentEntityException(String message, Throwable cause) {
