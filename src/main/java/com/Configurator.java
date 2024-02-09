dc35a1e22d2e4631962501d06ede7fe0b91992a5
Omar <omar@gmail.com>
2024-02-09 18:31:51 -0400

    Agregado api configuracion

--- src/main/java/com/Configurator.java
+++ src/main/java/com/Configurator.java
@@ -1,24 +1,66 @@
 package com;
 
-import com.opensource.Database.Generos;
-import com.opensource.Persistence.GenerosJpaController;
+import com.opensource.Database.*;
+import com.opensource.Persistence.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GenerosConfig {
+public class Configurator {
     
     @Bean
-    CommandLineRunner papa(GenerosJpaController repository){
+    CommandLineRunner Genero(GenerosJpaController repository){
         return args -> {
-            Generos mariam = new Generos(
-                    "Mariam",
-                    "mariam.jamal@gmail.com"                    
+            Generos m = new Generos(
+                    "Accion",
+                    "Activo"                    
             );
 
             repository.save(
-                    mariam
+                    m
+            );
+        };
+    }
+    
+    @Bean
+    CommandLineRunner Elenco(ElencoJpaController repository){
+        return args -> {
+            Elenco m = new Elenco(
+                    "Arnold Schwarzenegger",
+                    "Retirado"                    
+            );
+
+            repository.save(
+                    m
+            );
+        };
+    }
+    
+    @Bean
+    CommandLineRunner Idiomas(IdiomasJpaController repository){
+        return args -> {
+            Idiomas m = new Idiomas(
+                    "ingles",
+                    "Activo"                    
+            );
+
+            repository.save(
+                    m
+            );
+        };
+    }
+    
+    @Bean
+    CommandLineRunner Tipos(TiposArticulosJpaController repository){
+        return args -> {
+            TiposArticulos m = new TiposArticulos(
+                    "Digital",
+                    "Activo"                    
+            );
+
+            repository.save(
+                    m
             );
         };
     }
