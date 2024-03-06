8532146e7356dd4f1b4ebae18797ce7b74e8ee26
2003-Miguel <156967679+2003-Miguel@users.noreply.github.com>
2024-03-06 15:43:19 -0400

    Actualización

--- src/main/java/com/opensource/Database/Idiomas.java
+++ src/main/java/com/opensource/Database/Idiomas.java
@@ -2,10 +2,7 @@
 package com.opensource.Database;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
