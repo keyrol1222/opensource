3fb667e5a5c4aa47f10b2bb180e0286434f10c38
2003-Miguel <156967679+2003-Miguel@users.noreply.github.com>
2024-03-06 15:40:29 -0400

    Actualización

--- src/main/java/com/opensource/Database/Empleados.java
+++ src/main/java/com/opensource/Database/Empleados.java
@@ -2,12 +2,7 @@ package com.opensource.Database;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jakarta.persistence.*;
 import lombok.Data;
 
 
